9bb9f28000142144a8a9c7698ea5efc3b705aa7c
Salenback Petter <pettersalenback@gmail.com>
2023-02-25 11:52:01 +0100

    Initial commit

--- EventReview.BackendAPI/Program.cs
+++ EventReview.BackendAPI/Program.cs
@@ -17,8 +17,8 @@ namespace EventReview.BackendAPI
             // Add services to the container.
             builder.Services.AddSingleton<IRepository<Event>>(provider => 
             {
-                string endpointUri = "<ENDPOINT-URI>";
-                string primaryKey = "<PRIMARY-KEY>";
+                string endpointUri = "https://localhost:8081";
+                string primaryKey = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
                 var client = new CosmosClient(endpointUri, primaryKey);
 
                 var databaseName = "reviewDB";
